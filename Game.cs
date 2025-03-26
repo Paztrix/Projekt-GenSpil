92ec6bfe18e75730b98148f65c163b8a9ebb16c3
HE1935 <hmel71358@edu.ucl.dk>
2025-03-26 13:28:49 +0100

    Game class init

---
+++ Game.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+public class Game
+{
+    // private Felter/fields
+    private string gameName;
+    private int ageLimit;
+    private int gameVersion;
+
+
+
+
+    // Public properties
+    public string GameName
+    {
+        get { return gameName; }
+        set
+        {
+            gameName = value;
+        }
+    }
+
+
+    // Metode til at sætte aldergrænsen
+
+    public int AgeLimit
+    {
+        get { return ageLimit; }
+
+        set
+
+        {
+            if (value < 0)
+                Console.WriteLine("Forkert input, aldersgrænse kan ikke være negativ");
+            else
+
+                ageLimit = value;
+        }
+    }
+
+    public int GameVersion
+    {
+        get { return gameVersion; } //get method
+        set
+        {
+            gameVersion = value;  //Set method
+
+        }
+    }
+
+
+    // metode med spillets detaljer
+    public void ShowDetails()
+    {
+        Console.WriteLine($"Game: {GameName}, Age Limit: {AgeLimit} Game Version: {GameVersion}");
+    }
+}
